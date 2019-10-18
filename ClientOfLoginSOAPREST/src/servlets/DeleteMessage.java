275ace08804ab56318ad0e0c107ce505c4f203ea
Tommaso <tommasotroja@outlook.it>
2019-10-18 12:58:03 +0200

    correzione wsimport
    correzione wsimport buggato

---
+++ ClientOfLoginSOAPREST/src/servlets/DeleteMessage.java
@@ -0,0 +1,35 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import clients.MessagesSoapClient;
+import service.ws.ApplicationException_Exception;
+
+public class DeleteMessage extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		if ((boolean) request.getSession().getAttribute("isLogged")) {
+			int id = Integer.parseInt(request.getParameter("id"));
+			String friend = request.getParameter("friend");
+			try {
+				MessagesSoapClient.deleteMessage(id);
+				request.setAttribute("messageList",
+						MessagesSoapClient.getMessages((String) request.getSession().getAttribute("user"), friend));
+			} catch (ApplicationException_Exception e) {
+				e.printStackTrace();
+			}
+
+			getServletContext().getRequestDispatcher("/chat.jsp").forward(request, response);
+		} else
+			getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
+	}
+	
+}
