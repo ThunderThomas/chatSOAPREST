714c548909e81d0cacc01ba8d3225af2296fd0c5
Tommaso <tommasotroja@outlook.it>
2019-10-22 11:53:19 +0200

    Fix Timestamp

---
+++ ClientOfLoginSOAPREST/src/servlets/PostMessage.java
@@ -0,0 +1,36 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import clients.MessagesSoapClient;
+import service.ws.ApplicationException_Exception;
+
+public class PostMessage extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		if ((boolean) request.getSession().getAttribute("isLogged")) {
+			String destinatario = request.getParameter("destinatario");
+			String testo = request.getParameter("testo");
+			try {
+				MessagesSoapClient.addMessage((String) request.getSession().getAttribute("user"), destinatario, testo);
+				request.setAttribute("friend", destinatario);
+				request.setAttribute("messageList",
+						MessagesSoapClient.getMessages((String) request.getSession().getAttribute("user"), destinatario));
+				getServletContext().getRequestDispatcher("/chat.jsp").forward(request, response);
+			} catch (ApplicationException_Exception e) {
+				e.printStackTrace();
+			}
+		} else
+			getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
+	}
+
+}
